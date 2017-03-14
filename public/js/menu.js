655b1d8f6ee8370d036789b8e4941d2561635b0b
Oliver Berg <oliver.daniel.berg@gmail.com>
2017-03-14 21:26:59 +0100

    menu migrated to bootstrap with some stupid js adjustments ... working smoothly though. Closes #52

---
+++ public/js/menu.js
@@ -0,0 +1,26 @@
+const FONTSIZERATIO = 0.04;
+
+var adjustCss= function() {
+    var cw = $('.innerrow').width();
+    $('.innerrow').css({
+        'font-size':cw*FONTSIZERATIO+'px'
+    });
+    cw = $('.col-xs-4').width();
+    $('.menuIcon').css({
+        'width':cw+'px',
+        'height':cw+'px'
+    });
+
+    var marginVal = (window.innerHeight - $('.container').height()) / 2;
+    $('.container').css({
+        'margin-top':marginVal+'px'
+    });
+};
+
+
+window.onresize = function() {
+    adjustCss();
+}
+$('document').ready(function() {
+    adjustCss();
+});
